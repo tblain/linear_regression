9f07d4928059354e4a12e16d1fa4852480091d60
BLAIN THOMAS p1601248 <thomas.blain@etu.univ-lyon1.fr>
2019-03-25 20:05:20 +0100

    continuation

--- linear_regression.py
+++ linear_regression.py
@@ -10,13 +10,14 @@ class Model_regression:
         """
         self.bias = 1
         self.weights = np.random.rand(input_shape)
+        print(self.weights)
 
     def preactiv(self, x):
         return np.dot(self.weights, x) + self.bias
 
     def activ(self, res):
         return res
-        # return max(0, res)
+        #return max(0, res)
 
     def predict(self, x):
         return self.activ(self.preactiv(x))
@@ -27,8 +28,11 @@ class Model_regression:
             res[i] = self.predict(x.iloc[i])
         return res
 
-    def loss(self, res, y):
-        return y - res
+    def loss(self, y, res): # 1 example
+        #print("====")
+        #print(y-res)
+        #print(res)
+        return (res - y)**2
         # return ((y - res) ** 2) / 2
 
     def activation_deriv(self, res):
@@ -37,40 +41,72 @@ class Model_regression:
         else:
             return 0
 
-    def fit(self, x, y, step, batch_size, learning_rate, validation_datas):
+    def cost(self, res, y): # dataset
+        total_cost = 0
+        for p, t in zip(res, y):
+            print(p)
+            total_cost += np.dot(p, t)
+        return total_cost / res.shape[0]
+
+    def fit(self, x, y, step, epochs, batch_size, learning_rate, validation_datas):
         x_val = validation_datas[0]
         y_val = validation_datas[1]
+        x = x
+        y = y
+        i = 0
+        epo = 0
 
-        for i in tqdm(range(step)):
-            bx = x.loc[batch_size*i:batch_size*(i+1) - 1]
-            by = y.loc[batch_size*i:batch_size*(i+1) - 1]
+        for etape in range(step):
+            i += 1
+            bx = x.iloc[batch_size*i : batch_size*(i+1) - 1]
+            by = y.iloc[batch_size*i : batch_size*(i+1) - 1]
 
             w_gradient = np.zeros(self.weights.shape)
             b_gradient = 0
+            print("cost: ", self.cost(x, y))
 
             for k in range(len(bx)):
                 pre_a = self.preactiv(bx.iloc[k])
                 post_a = self.activ(pre_a)
-                loss = self.loss(post_a, by.iloc[k])
+                #print("============")
+                #print(post_a)
+                #print(bx.iloc[k])
+                #print(self.weights)
+                loss = self.loss(by.iloc[k], post_a)
                 act_deriv = 1 #self.activation_deriv(post_a)
                 w_gradient += loss * act_deriv * bx.iloc[k]
+                #print(w_gradient)
                 b_gradient += loss * act_deriv
 
-            self.weights -= learning_rate * w_gradient
-            self.bias    -= learning_rate * b_gradient
+            self.weights -= learning_rate * (1 / batch_size) * w_gradient
+            self.bias    -= learning_rate * (1 / batch_size) * b_gradient
 
-            # if i != 0 and i % 10 == 0:
-            if True:
-                print("Step: ", i)
-                # print(self.weights)
+            if x.shape[0] < batch_size*(i+1):
+                print(" permute: ", x.shape[0])
+                p = np.random.permutation(len(x))
+                x = x.iloc[p]
+                y = y.iloc[p]
+                i = 0
+
+            if etape != 0 and etape % (step / epochs) == 0:
+                print("=====================")
+            #if True:
+                #print(i * batch_size)
                 predic = self.predict_on_dataset(x_val)
-                print(((predic - y_val) **2).mean())
+                #for h, y in zip(predic, y_val):
+                    #pass
+                    #print("  ", h, " /  ", y)
+                print("Epoch: ", epo, "     loss: ", ((predic - y_val) **2).mean())
+                #print("predict: ", (predic - y_val).describe())
+                print("weights: ", self.weights)
+                print("bias: ", self.bias)
+                epo += 1
 
 
 dataset = pd.read_csv("./winequality-red.csv")
 
-train = dataset.head(1400)
-validation = dataset.tail(199)
+train = dataset
+#validation = dataset.tail(199)
 
 x_train = train.drop('quality', 1)
 y_train = train.quality
@@ -81,20 +117,19 @@ y_train = train.quality
 
 cali_dataframe = pd.read_csv("./california_housing_train.csv")
 x_train = cali_dataframe.drop("median_house_value", 1)
-y_train = cali_dataframe.median_house_value
+y_train = cali_dataframe.median_house_value / 1000
 
 def normalize(x):
     return (x-min(x))/(max(x)-min(x))
 
 for x in x_train:
-    x_train[x] = normalize(x_train[x])
+    #x_train[x] = normalize(x_train[x])
+    pass
 
-y_train = normalize(y_train)
 
 x_val = x_train.tail(500)
 y_val = y_train.tail(500)
 
-print(x_train.describe())
-
 model = Model_regression(x_train.shape[1])
-model.fit(x_train, y_train, step=100, batch_size=10, learning_rate=0.0000001, validation_datas=(x_val, y_val))
+
+model.fit(x_train, y_train, step=1000, epochs=10, batch_size=20, learning_rate=0.00000000000005, validation_datas=(x_val, y_val))
